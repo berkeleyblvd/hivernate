009f688cb7a42801d60405d4c6a15747361020dc
Nick Wichert <nick.wichert@icloud.com>
2023-12-19 16:10:02 -0600

    added google form script for form data capture v1

--- src/components/Application.vue
+++ src/components/Application.vue
@@ -9,7 +9,7 @@
     <div class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
       <div class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
-    <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+    <form @submit.prevent="submitForm" class="mx-auto mt-16 max-w-xl sm:mt-20">
       <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
         <div>
           <label for="first-name" class="block text-sm font-semibold leading-6 text-white">First name</label>
@@ -66,33 +66,49 @@
 
 </div>
 
-<!--
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-
-
-
-
 </template>
 
 <script>
-
 export default {
-  name: 'Application'
-}
+  name: 'Application',
+  methods: {
+    async submitForm() {
+      const formData = {
+        firstName: document.getElementById('first-name').value,
+        lastName: document.getElementById('last-name').value,
+        company: document.getElementById('company').value,
+        email: document.getElementById('email').value,
+        phoneNumber: document.getElementById('phone-number').value,
+        country: document.getElementById('country').value,
+        message: document.getElementById('message').value
+      };
 
+      try {
+        const response = await fetch('https://script.google.com/a/macros/berkeleyblvd.com/s/AKfycbwjflMDzM37lBwIgFLEWh-v0T1dh6qkfRogFsqsjWrqxAwzYYdsO5yGw3yUjz6Ynk2ncA/exec', {
+          method: 'POST',
+          mode: 'no-cors', // To handle CORS issues
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+          console.log('Form submitted successfully');
+          // Handle successful submission here (e.g., show a thank you message)
+        } else {
+          console.error('Form submission failed');
+          // Handle errors here
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        // Handle errors here
+      }
+    }
+  }
+}
 </script>
+
 
 <style>
 
