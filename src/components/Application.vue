ed25950154faecf42dab6308437380a159a4635c
Nick <nick.wichert@icloud.com>
2023-12-21 10:46:00 -0600

    updated blue accent color

--- src/components/Application.vue
+++ src/components/Application.vue
@@ -6,7 +6,7 @@
       <p class="mt-6 text-lg leading-8 text-gray-300">Unlock your business insights to build advantaged startups that create data not available anywhere else in the world.</p>
       <div class="mt-10">
         <a href="https://bd0hgjc84jc.typeform.com/to/GwTCQ47i">
-          <button class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+          <button class="block w-full rounded-md bg-sky-400 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
         </a>
       </div>
     </div>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,13 +3,13 @@
     <main>
       <div class="relative isolate">
         <div class="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48" aria-hidden="true">
-          <div class="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)" />
+          <div class="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#7dd3fc] to-[#0284c7] opacity-30" style="clip-path: polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)" />
         </div>
         <div class="overflow-hidden">
           <div class="mx-auto max-w-7xl px-6 pb-32 pt-16 sm:pt-60 lg:px-8 lg:pt-16">
             <div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
               <div class="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-                <h1 class="text-6xl font-bold text-white sm:text-6xl">We translate business insights into venture investible startups</h1>
+                <h1 class="text-6xl font-bold text-sky-400 sm:text-6xl">We translate business insights into venture investible startups</h1>
                 <p class="mt-6 text-lg leading-8 text-gray-400 sm:max-w-md lg:max-w-none">We are a complement to corporate innovation, a partner to corporate venture, and a collaborator to future corporate strategy. We believe that we never lose, we win or we learn; we’re quick learners and seasoned integrators. New ventures are experiments designed to create data that doesn't exist anywhere else in the world.</p>
               </div>
               <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
